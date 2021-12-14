ce12e4797fd0ef8d870ae954fa5ddefbb60a3208
maeve <ducktyping@kakao.com>
2021-12-15 03:49:12 +0900

    Kopring Init Setting - Spring Data Jpa - Spring Security - H2 Database

---
+++ src/main/kotlin/io/social/socialcommunity/SocialCommunityApplication.kt
@@ -0,0 +1,11 @@
+package io.social.socialcommunity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SocialCommunityApplication
+
+fun main(args: Array<String>) {
+	runApplication<SocialCommunityApplication>(*args)
+}
---
+++ src/test/kotlin/io/social/socialcommunity/SocialCommunityApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.social.socialcommunity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SocialCommunityApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
